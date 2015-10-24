229c004e6cd6e3b4fd6123aa23684574c6a10c9a
efrenaguilar95 <efrenaguilar95@hotmail.com>
2015-10-23 23:50:38 -0700

    DFS basic
    Implemented a super basic dfs. Check it out, any ways you can find to
    make it better/cleaner would be awesome.
    I also hope run more tests on randomly generated trees to make sure it
    always works properly.

--- GameTree/main.cpp
+++ GameTree/main.cpp
@@ -1,16 +1,25 @@
 #include "gametree.h"
 
+
+
 int main()
 {
 	//Random comment
 	GameTree<int> gTree(10);
-	gTree.print_value();
+	//gTree.print_value();
 	GameTree<int> gTree2(8);
 	//GameTree<int>* g2 = &gTree2;
 	gTree.addChild(gTree2);
-	gTree.first_child->print_value();
+	//gTree.first_child->print_value();
 	GameTree<int> gTree3(7);
+	GameTree<int> gTree4(14);
+	GameTree<int> gTree5(25);
 	gTree.first_child->addSibling(gTree3);
-	std::cout << gTree.first_child->next_sibling->value<< std::endl;
-
+	gTree.first_child->addChild(gTree4);
+	gTree.first_child->next_sibling->addChild(gTree5);
+	//std::cout << gTree.first_child->next_sibling->value<< std::endl;
+	gTree.dfs();
 }
+
+
+
